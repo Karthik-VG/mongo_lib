0159d2df1fae64d61cfca9183c03dabea7ebb930
Karthik-VG <karthikvg.engineer@gmail.com>
2022-03-20 22:48:00 +0530

    added install_requires in setup.py

--- setup.py
+++ setup.py
@@ -27,4 +27,12 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires = [
+        "pymongo==2.8",
+        "pandas",
+        "json",
+        "dns",
+        "sys"
+    ]
+
 )
