5fef9ddfb0ec196748c6288cda97a8d7153c0961
Karthik-VG <karthikvg.engineer@gmail.com>
2022-03-21 18:51:42 +0530

    adding version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ USER_NAME = "Karthik-VG"
 
 setuptools.setup(
     name= f"{PROJECT_NAME}",
-    version="0.0.4",
+    version="1.0.1",
     author=USER_NAME,
     author_email="karthikvg.engineer@gmail.com",
     description="Its a implementation of mongodB operations for atlas",
